6c772029323153872ce8956d59f92968a277e34b
Ivan Milincic <kreativan@outlook.com>
2019-03-12 18:28:28 +0100

    move _lang.php to templates root

---
+++ templates/_lang.php
@@ -0,0 +1,14 @@
+<?php namespace ProcessWire;
+
+$lng___read_more            = __('Read More');
+
+// contact
+$lng___name                 = __('Name');
+$lng___email                = __('Email');
+$lng___subject              = __('Subject');
+$lng___message              = __('Message');
+$lng___send                 = __('Send');
+$lng___send_message         = __('Send Message');
+$lng___contact              = __('Contact');
+$lng___contact_us           = __('Contact Us');
+$lng___form_message         = __('Your message has been sent. Thank you for your time.');

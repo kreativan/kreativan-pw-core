d7a69ed0e767dd7ed168933a106b1d5b8a108cbb
Ivan Milincic <kreativan@outlook.com>
2019-03-22 12:19:10 +0100

    added evenOdd() and timeAgo() functions

--- templates/helpers/_func.php
+++ templates/helpers/_func.php
@@ -9,6 +9,9 @@
  *	@method loadJS($js)
  *  @method loadCSS($js)
  *
+ *  @method evenOdd($i++)
+ *  @method timeAgo(@time_stamp)
+ *
 */
 
 // load js
@@ -18,4 +21,54 @@ function loadJS($js) {
 // load css
 function loadCSS($css) {
     wire('config')->styles->add($css);
+}
+
+
+/**
+ *  Even/Odd
+ *
+ */
+function evenOdd($number) {
+    if ($number % 2 == 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+
+/**
+ *  Time Ago
+ *  @param some date
+ *  @example timeAgo(@time_stamp);
+ *  @example timeAgo(2019-05-01 00:22:35);
+ *  @example timeAgo('2019-05-01 00:22:35', true);
+ *
+ */
+function timeAgo($datetime, $full = false) {
+
+    $now = new \DateTime;
+    $ago = new \DateTime($datetime);
+    $diff = $now->diff($ago);
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+
 }

f7e19982fa5b33d3818b3ab9a33fddc9e4e1d7e0
Ivan Milincic <kreativan@outlook.com>
2019-08-31 12:46:45 +0200

    update less

--- templates/shortcodes/form-builder.php
+++ templates/shortcodes/form-builder.php
@@ -6,7 +6,10 @@
 
 if(strpos($_SERVER['REQUEST_URI'], $this->wire('config')->urls->admin) !== 0) {
 
+    $class .= ($card == "true") ? "uk-card uk-card-default uk-card-body" : "";
+
     echo "<div class='$class'>";
+    echo !empty($headline) ? "<h3>$headline</h3>" : "";
     $form = $forms->render("$form_name");
     echo $form->styles;
     echo $form->scripts;
